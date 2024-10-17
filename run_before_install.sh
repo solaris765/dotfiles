6f0e483985a26fb6d798e1530ecd8a7f1f14c8cf
Mason Rhodes <mason@lifemd.com>
2024-10-17 14:08:11 -0700

    wip: ssh stuff

---
+++ run_before_install.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+if ! command -v bw &> /dev/null; then  
+  echo "Installing Bitwarden-CLI"
+  sudo dnf install curl jq
+  
+  REPO="bitwarden/clients"
+  ASSET_PATTERN="bw-linux-.*\.zip"
+  
+  # Fetch all releases
+  releases=$(curl -s "https://api.github.com/repos/$REPO/releases")
+  
+  # Find the latest release with a tag starting with "CLI"
+  latest_cli_release=$(echo "$releases" | jq -r '.[] | select(.tag_name | startswith("cli-")) | .tag_name' | sort -V | tail -n1)
+  
+  if [ -z "$latest_cli_release" ]; then
+      echo "No CLI release found."
+      exit 1
+  fi
+  
+  echo "Latest CLI release: $latest_cli_release"
+  
+  # Fetch the specific release
+  release_info=$(curl -s "https://api.github.com/repos/$REPO/releases/tags/$latest_cli_release")
+  
+  # Extract the download URL for the matching asset
+  download_url=$(echo "$release_info" | jq -r --arg PATTERN "$ASSET_PATTERN" '.assets[] | select(.name | test($PATTERN)) | .browser_download_url')
+  
+  if [ -z "$download_url" ]; then
+      echo "No matching asset found in the latest CLI release."
+      exit 1
+  fi
+  
+  # Extract the filename from the URL
+  filename=$(basename "$download_url")
+  
+  # Download the asset
+  echo "Downloading $filename..."
+  curl -L -o "$filename" "$download_url"
+  
+  echo "Download complete: $filename"
+  
+  unzip $filename
+  rm $filename
+  sudo mv bw /usr/local/bin/bw
+  sudo chmod +x /usr/local/bin/bw
+fi
+
+function bwu() {
+    BW_STATUS=$(bw status | jq -r .status)
+    case "$BW_STATUS" in
+    "unauthenticated")
+        echo "Logging into BitWarden"
+        export BW_SESSION=$(bw login --raw)
+        ;;
+    "locked")
+        echo "Unlocking Vault"
+        export BW_SESSION=$(bw unlock --raw)
+        ;;
+    "unlocked")
+        echo "Vault is unlocked"
+        ;;
+    *)
+        echo "Unknown Login Status: $BW_STATUS"
+        return 1
+        ;;
+    esac
+    bw sync
+}
+bwu
--- run_onchange_packages.sh
+++ run_onchange_packages.sh
@@ -1,22 +1,31 @@
 #!/bin/sh
 
 # Utils
-echo "Installing Utils"
-sudo dnf install vim curl git jq seahorse Thunar solaar wireplumber NetworkManager
+if ! command -v vim &> /dev/null || ! command -v curl &> /dev/null || ! command -v git &> /dev/null || ! command -v jq &> /dev/null || ! command -v seahorse &> /dev/null || ! command -v Thunar &> /dev/null || ! command -v solaar &> /dev/null || ! command -v wireplumber &> /dev/null || ! command -v NetworkManager &> /dev/null; then
+    echo "Installing Utils"
+    sudo dnf install -y vim curl git jq seahorse Thunar solaar wireplumber NetworkManager
+fi
 
 # Terminal
-sudo dnf install foot tmux
+if ! command -v foot &> /dev/null || ! command -v tmux &> /dev/null; then
+    echo "Installing Terminal tools"
+    sudo dnf install -y foot tmux
+fi
 
 # Node
-echo "Installing NodeJS tooling"
-sudo dnf install node
-curl -fsSL https://bun.sh/install | bash
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+if ! command -v node &> /dev/null || ! command -v bun &> /dev/null || ! command -v nvm &> /dev/null; then
+    echo "Installing NodeJS tooling"
+    sudo dnf install -y node
+    curl -fsSL https://bun.sh/install | bash
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+fi
 
 # hyprland
-echo "Installing Hyprland Tooling"
-sudo dnf install hyprcursor.x86_64 hyprcursor-devel.x86_64 hypridle.x86_64 hyprland.x86_64 hyprland-devel.x86_64 hyprland-protocols-devel.noarch hyprlang.x86_64 hyprlang-devel.x86_64 hyprlock.x86_64 hyprpicker.x86_64 hyprutils.x86_64 hyprutils-devel.x86_64 hyprwayland-scanner-devel.x86_64 xdg-desktop-portal-hyprland.x86_64 
-sudo dnf install wofi waybar
+if ! command -v hyprland &> /dev/null; then
+    echo "Installing Hyprland Tooling"
+    sudo dnf install -y hyprcursor.x86_64 hyprcursor-devel.x86_64 hypridle.x86_64 hyprland.x86_64 hyprland-devel.x86_64 hyprland-protocols-devel.noarch hyprlang.x86_64 hyprlang-devel.x86_64 hyprlock.x86_64 hyprpicker.x86_64 hyprutils.x86_64 hyprutils-devel.x86_64 hyprwayland-scanner-devel.x86_64 xdg-desktop-portal-hyprland.x86_64 
+    sudo dnf install -y wofi waybar
+fi
 
 # Helper
 ghRepoCloneLatestRelease ()
@@ -27,53 +36,57 @@ ghRepoCloneLatestRelease ()
                 tar --strip-components=1 -xzv >/dev/null
 }
 
-
 # Local Software
-echo "Installing github software builds"
-mkdir -p ~/.git-software
-cd ~/.git-software
+if ! command -v swww &> /dev/null; then
+    echo "Installing github software builds"
+    mkdir -p ~/.git-software
+    cd ~/.git-software
 
-## swww
-echo "  SWWW"
-ghRepoCloneLatestRelease LGFae/swww
-cd swww
-cargo build --release
-ln -s $HOME/.git-software/swww/target/release/swww $HOME/.local/bin/swww
-ln -s $HOME/.git-software/swww/target/release/swww-daemon $HOME/.local/bin/swww-daemon
-cd ..
-
-
+    ## swww
+    echo "  SWWW"
+    ghRepoCloneLatestRelease LGFae/swww
+    cd swww
+    cargo build --release
+    ln -s $HOME/.git-software/swww/target/release/swww $HOME/.local/bin/swww
+    ln -s $HOME/.git-software/swww/target/release/swww-daemon $HOME/.local/bin/swww-daemon
+    cd ..
+fi
 
 cd ~
 
 # VSCode
-echo "Installing VSCode"
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+if ! command -v code &> /dev/null; then
+    echo "Installing VSCode"
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 
-dnf check-update
-sudo dnf install code # or code-insiders
+    dnf check-update
+    sudo dnf install -y code # or code-insiders
+fi
 
 # browser
-echo "Installing Web Browser"
-sudo dnf install dnf-plugins-core
-sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-sudo dnf install brave-browser
+if ! command -v brave-browser &> /dev/null; then
+    echo "Installing Web Browser"
+    sudo dnf install -y dnf-plugins-core
+    sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+    sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+    sudo dnf install -y brave-browser
 
-echo "### Installing bitwarden browser plugin"
-BITWARDEN_ID=nngceckbapebfimnlniiiahkandclblb
-EXTENSIONS_PATH=/opt/brave.com/brave/extensions
-mkdir -p $EXTENSIONS_PATH
-echo '{ "external_update_url": "https://clients2.google.com/service/update2/crx" }' > "${EXTENSIONS_PATH}/${BITWARDEN_ID}.json"
+    echo "### Installing bitwarden browser plugin"
+    BITWARDEN_ID=nngceckbapebfimnlniiiahkandclblb
+    EXTENSIONS_PATH=/opt/brave.com/brave/extensions
+    mkdir -p $EXTENSIONS_PATH
+    echo '{ "external_update_url": "https://clients2.google.com/service/update2/crx" }' > "${EXTENSIONS_PATH}/${BITWARDEN_ID}.json"
+fi
 
 # PriTunl
 echo "Prepping VPN"
 xdg-open https://vpn.lifemd.io/sso/request
 
 # MongoDB
-echo "Installing Mongodb"
-sudo cat <<EOF > /etc/mongod.conf
+if ! command -v mongod &> /dev/null; then
+    echo "Installing Mongodb"
+    sudo cat <<EOF > /etc/mongod.conf
 # mongod.conf
 
 # for documentation of all options, see:
@@ -113,7 +126,7 @@ replication:
 #auditLog:
 EOF
 
-sudo cat <<EOF > /etc/yum.repos.d/mongodb-org-8.0.repo
+    sudo cat <<EOF > /etc/yum.repos.d/mongodb-org-8.0.repo
 [mongodb-org-8.0]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/amazon/2023/mongodb-org/8.0/x86_64/
@@ -122,34 +135,39 @@ enabled=1
 gpgkey=https://pgp.mongodb.com/server-8.0.asc
 EOF
 
-sudo dnf install -y mongodb-org
-sudo systemctl start mongod
-sudo systemctl status mongod
-sudo systemctl enable mongod
-mongosh --eval "rs.initiate({_id: "rs0",version: 1,members: [{ _id: 0, host : "localhost:27017" }]}))"
+    sudo dnf install -y -y mongodb-org
+    sudo systemctl start mongod
+    sudo systemctl enable mongod
+    mongosh --eval "rs.initiate({_id: \"rs0\",version: 1,members: [{ _id: 0, host : \"localhost:27017\" }]})"
+fi
 
 #slack 
-echo "Installing Slack"
-cd ~/Downloads
-curl https://slack.com/downloads/instructions/linux?ddl=1&build=rpm --output slack.rpm
-sudo dnf install ./slack.rpm
+if ! command -v slack &> /dev/null; then
+    echo "Installing Slack"
+    cd ~/Downloads
+    curl https://slack.com/downloads/instructions/linux?ddl=1&build=rpm --output slack.rpm
+    sudo dnf install -y ./slack.rpm
+fi
 
 # docker
-echo "Installing Docker"
-sudo dnf -y install dnf-plugins-core
-sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo systemctl start docker
+if ! command -v docker &> /dev/null; then
+    echo "Installing Docker"
+    sudo dnf -y install dnf-plugins-core
+    sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+    sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo systemctl start docker
 
-sudo groupadd docker
-sudo usermod -aG docker $USER
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
 
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
-
+    sudo systemctl enable docker.service
+    sudo systemctl enable containerd.service
+fi
 
 #gcm
-echo "Installing GCM"
-curl -L https://aka.ms/gcm/linux-install-source.sh | sh
-git-credential-manager configure
-git config --global credential.credentialStore secretservice
+if ! command -v git-credential-manager &> /dev/null; then
+    echo "Installing GCM"
+    curl -L https://aka.ms/gcm/linux-install-source.sh | sh
+    git-credential-manager configure
+    git config --global credential.credentialStore secretservice
+fi
