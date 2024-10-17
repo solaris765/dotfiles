f4cace7c30e09dc3e6f8481cd24b0463eddaab18
Mason Rhodes <mason@lifemd.com>
2024-10-17 09:52:23 -0700

    fix: pwd mngr install

--- run_once_before_install-password-manager.sh
+++ run_once_before_install-password-manager.sh
@@ -3,4 +3,6 @@
 # exit immediately if password-manager-binary is already in $PATH
 type bw >/dev/null 2>&1 && exit
 
-npm install -g @bitwarden/cli
+curl "https://vault.bitwarden.com/download/?app=cli&platform=linux" --output bw
+sudo mv bw /usr/local/bin/bw
+sudo chmod +x /usr/local/bin/bw
