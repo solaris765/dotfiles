9dc4f6afaa9397830a810b06c20755d0ec751876
Mason Rhodes <mason@lifemd.com>
2024-10-18 16:02:15 -0700

    fix: fira

--- software_installers/00_utils.sh
+++ software_installers/00_utils.sh
@@ -3,6 +3,6 @@ set -eu
 
 # Utils
 echo "Installing Utils"
-sudo dnf install -y vim curl git jq Thunar solaar wireplumber NetworkManager go gcc-go keychain powerline-fonts
+sudo dnf install -y vim curl git jq Thunar solaar wireplumber NetworkManager go gcc-go keychain powerline-fonts fira-code-fonts
 
 exit 0
