6975e9f547324a866b85e2334e2bb91b107683b1
Mason Rhodes <mason@lifemd.com>
2024-10-18 14:02:01 -0700

    fix: try dnf

--- software_installers/09_slack.sh
+++ software_installers/09_slack.sh
@@ -4,13 +4,17 @@ set -eu
 #slack
 if ! command -v slack &> /dev/null; then
     echo "Installing Slack"
-    filename="~/Downloads/slack.rpm"
-    
-    if curl --silent -o "${filename}" -L "https://slack.com/downloads/instructions/linux?ddl=1&build=rpm"; then
-        sudo dnf install -y ~/Downloads/slack.rpm
-    else
-        exit 1
-    fi
+        sudo bash -c 'cat <<EOF > /etc/yum.repos.d/slack.repo
+[slack]
+name=slack
+baseurl=https://packagecloud.io/slacktechnologies/slack/fedora/21/x86_64
+enabled=1
+gpgcheck=0
+gpgkey=https://packagecloud.io/gpg.key
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+EOF'
+    sudo dnf install -y slack
 fi
 
 exit 0
