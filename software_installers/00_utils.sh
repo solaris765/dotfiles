49d4c4e26ced1dc02c02d2536a960f5cc885427b
Mason Rhodes <mason@lifemd.com>
2024-10-18 13:43:36 -0700

    fix: add go as a util

--- software_installers/00_utils.sh
+++ software_installers/00_utils.sh
@@ -4,7 +4,7 @@ set -eu
 # Utils
 if ! command -v vim &> /dev/null || ! command -v curl &> /dev/null || ! command -v git &> /dev/null || ! command -v jq &> /dev/null || ! command -v seahorse &> /dev/null || ! command -v Thunar &> /dev/null || ! command -v solaar &> /dev/null || ! command -v wireplumber &> /dev/null || ! command -v NetworkManager &> /dev/null; then
     echo "Installing Utils"
-    sudo dnf install -y vim curl git jq seahorse Thunar solaar wireplumber NetworkManager
+    sudo dnf install -y vim curl git jq seahorse Thunar solaar wireplumber NetworkManager go
 fi
 
 exit 0
