5dccc8c3a451908b584b0fb01f39a3b0e8acd727
Mason Rhodes <mason@lifemd.com>
2024-10-18 12:27:02 -0700

    fix: sudo cat

--- software_installers/08_mongo.sh
+++ software_installers/08_mongo.sh
@@ -4,7 +4,7 @@ set -eu
 # MongoDB
 if ! command -v mongod &> /dev/null; then
     echo "Installing Mongodb"
-    sudo cat <<EOF > /etc/mongod.conf
+    sudo bash -c 'cat <<EOF > /etc/mongod.conf
 # mongod.conf
 
 # for documentation of all options, see:
@@ -42,16 +42,16 @@ replication:
 ## Enterprise-Only Options
 
 #auditLog:
-EOF
+EOF'
 
-    sudo cat <<EOF > /etc/yum.repos.d/mongodb-org-8.0.repo
+    sudo bash -c 'cat <<EOF > /etc/yum.repos.d/mongodb-org-8.0.repo
 [mongodb-org-8.0]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/amazon/2023/mongodb-org/8.0/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://pgp.mongodb.com/server-8.0.asc
-EOF
+EOF'
 
     sudo dnf install -y -y mongodb-org
     sudo systemctl start mongod
