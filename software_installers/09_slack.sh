46ab4a2b8aa8bbcfeaefc4b44163839d833ea1f6
Mason Rhodes <mason@lifemd.com>
2024-10-18 12:29:08 -0700

    fix: quote your curls

--- software_installers/09_slack.sh
+++ software_installers/09_slack.sh
@@ -5,7 +5,7 @@ set -eu
 if ! command -v slack &> /dev/null; then
     echo "Installing Slack"
     cd ~/Downloads
-    curl https://slack.com/downloads/instructions/linux?ddl=1&build=rpm --output slack.rpm
+    curl "https://slack.com/downloads/instructions/linux?ddl=1&build=rpm" --output slack.rpm
     sudo dnf install -y ./slack.rpm
 fi
 
